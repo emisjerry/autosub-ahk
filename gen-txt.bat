a11653fcc4db98418ce94685cd9168d3b32c76b0
jerry <emisjerry@gmail.com>
2020-05-14 23:59:32 +0800

    add gen-txt.bat

---
+++ gen-txt.bat
@@ -0,0 +1,13 @@
+rem @echo off
+echo *** Loading AutoSub, please wait ...
+set LANG=%1
+set INPUT=%2
+set OUTPUT=%3
+set REAL_OUTPUT=%4
+echo %DATE% %TIME% autosub -S %LANG% -i %INPUT% -o %OUTPUT% -F txt > gen.log
+rem Append -F txt, if you want a text output
+cd autosub
+autosub -S %LANG% -i %INPUT% -o %OUTPUT% -et 50 -mxrs 6 -F txt
+cd ..
+rem Open the output
+%REAL_OUTPUT%
